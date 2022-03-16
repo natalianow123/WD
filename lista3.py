7e2a311d99ebd884196f13b445aed5294c21f6e5
natalianow123 <97948947+natalianow123@users.noreply.github.com>
2022-03-16 10:28:45 +0100

    Add files via upload

---
+++ lista3.py
@@ -0,0 +1,12 @@
+import random
+# zad 1
+# A=[1-x for x in range(1,11)]
+# B=[4**x for x in range(0,8)]
+# C=[x for x in B if x%2==0]
+#
+# zad 2
+lista1=[]
+for x in range(0,10):
+    lista1[x]=
+
+print(lista1)
